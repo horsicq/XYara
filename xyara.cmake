0cf39006f97d206a5157e1314f467f23d98329a0
horsicq <horsicq@gmail.com>
2023-10-17 18:21:25 +0200

    Update file(s): 2023-10-17

--- xyara.cmake
+++ xyara.cmake
@@ -1,5 +1,5 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
-
+include_directories(${CMAKE_CURRENT_LIST_DIR}/3rdparty/yara/src/include/)
 
 set(XYARA_SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/xyara.cpp
